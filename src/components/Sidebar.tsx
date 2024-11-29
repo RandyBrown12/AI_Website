924f2c8e8d923aee0d8c675cf3cfa65a1249bac4
Randy <randybrown9812@gmail.com>
2024-11-29 15:15:05 -0600

    Add Chat Exports to a given chat

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { SidebarProps, Data } from './interfaces';
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MessageIcon from '@mui/icons-material/Message';
+import FileDownload from '@mui/icons-material/FileDownload';
 
 export const Sidebar = ({isSidebarShown, showSidebar, userData, setUserData, currentChatId, setCurrentChatId}: SidebarProps) => {
   
@@ -19,6 +20,20 @@ export const Sidebar = ({isSidebarShown, showSidebar, userData, setUserData, cur
 
   const handleClearChat = () => {
     setCurrentChatId('');
+  }
+
+  const exportTextFile = () => {
+    const currentData = userData.find(chat => chat.id === currentChatId);
+    if(!currentData) {
+      return;
+    }
+    console.table(currentData.chat);
+    const blob = new Blob([JSON.stringify(currentData.chat)], { type: "application/json" });
+    const link = document.createElement("a");
+    link.download = "output_history.txt";
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
   }
 
   return (
@@ -59,9 +74,14 @@ export const Sidebar = ({isSidebarShown, showSidebar, userData, setUserData, cur
             <ListItem
               key={item.id}
               secondaryAction={
-                <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(item.id)}>
-                  <DeleteIcon />
-                </IconButton>
+                <>
+                  <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(item.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                  <IconButton edge="end" onClick={() => exportTextFile()}>
+                    <FileDownload />
+                  </IconButton>
+                </>
               }>
               <ListItemText
                 primary={item.title}
